1f512fc26437d78c9acbd3497c189fbd9b9d3bce
jessicaxiao11 <jessicaxiao11@gmail.com>
2021-10-23 15:35:59 -0400

    commented js file

--- homework_6a/script.js
+++ homework_6a/script.js
@@ -1,3 +1,4 @@
+// Show and hide website menu
 function showMenu() {
   document.getElementById("overlay").style.left = "0";
 }
@@ -6,16 +7,19 @@ function hideMenu() {
   document.getElementById("overlay").style.left = "-25%";
 }
 
+// Transition to first quiz question on home page
 function showQ1() {
   document.getElementById("quizIntro").style.display = "none";
   document.getElementById("quizQ1").style.display = "block";
 }
 
+// Transition to second quiz question on home page
 function showQ2() {
   document.getElementById("quizQ1").style.display = "none";
   document.getElementById("quizQ2").style.display = "block";
 }
 
+// Show and hide section menu on product browsing page
 function showSideMenu() {
   document.getElementById("sectionMenuOverlay").style.left = "0";
 }
@@ -24,7 +28,9 @@ function hideSideMenu() {
   document.getElementById("sectionMenuOverlay").style.left = "-25%";
 }
 
-/* scrolling code from:
+/* Scroll to section on product browsing page after using the section menu
+ * 
+ * scrolling code from:
  * https://stackoverflow.com/questions/24665602/scrollintoview-scrolls-just-too-far
  */
 function scrollToElement(id) {
@@ -34,12 +40,14 @@ function scrollToElement(id) {
   window.scrollTo({top: y, behavior: 'smooth'});
 }
 
+// Increase number of items in cart by one
 function addToCart() {
   var numInCart = sessionStorage.getItem('numInCart');
   sessionStorage.setItem('numInCart', parseInt(numInCart)+1);
   updateCart();
 }
 
+// Update displayed number of items in cart
 function updateCart() {
   var numInCart = sessionStorage.getItem('numInCart');
   if (numInCart === null) {
@@ -53,10 +61,12 @@ function updateCart() {
   }
 }
 
+// Display correct number of items in cart when window first loads
 window.onload = function(){ 
   updateCart(); 
 }
 
+// Select size in product detail page, by greying out un-selected items
 function selectSize(id) {
   var sizes = ['tiny', 'small', 'medium', 'large'];
   document.getElementById(id).style.opacity = "100%";
@@ -67,20 +77,28 @@ function selectSize(id) {
   }
 }
 
+/* Select color in product detail page, by greying out un-selected items
+ * Color selection also determines whether the item is in low stock, and which 
+ * product image is displayed */
 function selectColor(id) {
   var sizes = ['strawberry', 'blackberry', 'crazyberry', 'fireorange'];
   document.getElementById(id).style.opacity = "100%";
+  
+  // grey out unselected items
   for (var i = 0; i < sizes.length; i++) {
     if (sizes[i] !== id) {
       document.getElementById(sizes[i]).style.opacity = "20%";
     }
   }
+
+  // display low stock for the blackberry color
   if (id !== "blackberry") {
     document.getElementById("lowStock").style.visibility = "hidden";
   } else {
     document.getElementById("lowStock").style.visibility = "visible";
   }
 
+  // show correct product image that corresponds with the selected color
   var color = null;
   if (id == "blackberry") {
     color = 1;
