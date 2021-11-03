ba32d4320ad6be7c8341989578d85df90b0d3214
jessicaxiao11 <jessicaxiao11@gmail.com>
2021-11-03 17:51:50 -0400

    add versions of product to shopping cart

--- homework_6b/script.js
+++ homework_6b/script.js
@@ -40,15 +40,52 @@ function scrollToElement(id) {
   window.scrollTo({top: y, behavior: 'smooth'});
 }
 
-// Increase number of items in cart by one
-function addToCart() {
-  var numInCart = sessionStorage.getItem('numInCart');
-  sessionStorage.setItem('numInCart', parseInt(numInCart)+1);
-  updateCart();
+
+
+/* ------------------   Shopping Cart Functionality ------------------------ */
+
+// Object for product added to cart
+function Product(productSize, productColor) {
+  this.name = "Furdinand Backpack";
+  this.size = productSize;
+  this.color = productColor;
+  this.quanity = 1;
+  this.price = "$95";
+  this.increment = function() { this.size++; };
 }
 
-// Update displayed number of items in cart
-function updateCart() {
+// Add item to cart in model
+function addToCart() {
+  // Increase number in cart
+  var numInCart = sessionStorage.getItem('numInCart');
+  sessionStorage.setItem('numInCart', parseInt(numInCart)+1);
+
+  // Add item to cart array
+  var cart = JSON.parse(sessionStorage.getItem('cart'));
+  if (cart === null) {
+    cart = [];
+  }
+  var itemExists = false;
+  for (var i = 0; i < cart.length; i++) {
+    var item = cart[i];
+    if (item.size === selectedSize && item.color === selectedColor) {
+      item.increment();
+      itemExists = true;
+    }
+  }
+
+  if (!itemExists) {
+    var newItem = new Product(selectedSize, selectedColor);
+    cart.push(newItem);
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  // Update view
+  updateCartNum();
+}
+
+// Update view of cart number
+function updateCartNum() {
   var numInCart = sessionStorage.getItem('numInCart');
   if (numInCart === null) {
     sessionStorage.setItem('numInCart', 0);
@@ -61,17 +98,74 @@ function updateCart() {
   }
 }
 
-// Display correct number of items in cart when window first loads
-window.onload = function(){ 
-  updateCart(); 
+// Display cart items
+function displayCart() {
+  // remove all children
+  var cartItems = document.getElementById("cartItems");
+
+  // display all children
+  var cart = JSON.parse(sessionStorage.getItem('cart'));
+  if (cart === null) {
+    cart = [];
+  }
+  for (var i = 0; i < cart.length; i++) {
+    // create new item
+    const item = document.createElement("div");
+    cartItems.appendChild(item);
+
+    // create img of item
+    const img = document.createElement("img");
+    img.className = "cartItemImage";
+    img.src = "images/furdinandBackpack1.png";
+    img.alt = "image of furdinand backpack";
+    item.appendChild(img);
+
+    // create title, size, color, price, quantity
+    const title = document.createElement("p");
+    title.className = "cartItemTitle";
+    title.innerHTML = "Furdinand Backpack";
+    item.appendChild(title);
+
+    const size = document.createElement("p");
+    size.className = "cartItemSize";
+    size.innerHTML = "Size: " + cart[i].size;
+    item.appendChild(size);
+
+    const color = document.createElement("p");
+    color.className = "cartItemColor";
+    color.innerHTML = "Color: " + cart[i].color;
+    item.appendChild(color);
+
+    const price = document.createElement("p");
+    price.className = "cartItemPrice";
+    price.innerHTML = cart[i].price;
+    item.appendChild(price);
+
+    const quantity = document.createElement("p");
+    quantity.className = "cartItemQuantity";
+    quantity.innerHTML = cart[i].quantity;
+    item.appendChild(quantity);
+  }
 }
+
+// Display correct items in cart when window first loads
+window.onload = function(){ 
+  updateCartNum(); 
+  if (window.location.href.indexOf('cart.html') > -1) {
+    displayCart();
+  }
+}
+
+var selectedSize = 'tiny';
+var selectedColor = 'blackberry';
 
 // Select size in product detail page, by greying out un-selected items
 function selectSize(id) {
   var sizes = ['tiny', 'small', 'medium', 'large'];
+  selectedSize = id;
   document.getElementById(id).style.opacity = "100%";
   for (var i = 0; i < sizes.length; i++) {
-    if (sizes[i] !== id) {
+    if (sizes[i] !== selectedSize) {
       document.getElementById(sizes[i]).style.opacity = "20%";
     }
   }
@@ -82,17 +176,17 @@ function selectSize(id) {
  * product image is displayed */
 function selectColor(id) {
   var sizes = ['strawberry', 'blackberry', 'crazyberry', 'fireorange'];
+  selectedColor = id;
   document.getElementById(id).style.opacity = "100%";
-  
   // grey out unselected items
   for (var i = 0; i < sizes.length; i++) {
-    if (sizes[i] !== id) {
+    if (sizes[i] !== selectedColor) {
       document.getElementById(sizes[i]).style.opacity = "20%";
     }
   }
 
   // display low stock for the blackberry color
-  if (id !== "blackberry") {
+  if (selectedColor !== "blackberry") {
     document.getElementById("lowStock").style.visibility = "hidden";
   } else {
     document.getElementById("lowStock").style.visibility = "visible";
